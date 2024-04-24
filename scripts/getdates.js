93d46e3de125319bcf68de643d14a4cba156ee24
hwcall1102 <haydenc0293@gmail.com>
2024-04-23 23:47:25 -0600

    new home site

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+
+const today = new Date();
+
+const year = document.querySelector("#currentyear");
+year.innerHTML = `<span>${today.getFullYear()}</span>`;
+
+const modified = document.querySelector("#lastModified");
+modified.innerHTML = `Last Modification <span>${document.lastModified}</span>`;
