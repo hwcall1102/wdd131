3d19d80e7358e3b94cc8affb50cda1fa1bc99483
hwcall1102 <haydenc0293@gmail.com>
2024-05-07 23:53:10 -0600

    javascript

--- scripts/place.js
+++ scripts/place.js
@@ -7,3 +7,25 @@ year.innerHTML = `<span>${today.getFullYear()}</span>`;
 const modified = document.querySelector("#lastModified");
 modified.innerHTML = `Last Modification <span>${document.lastModified}</span>`;
 //
+
+function calculateWindChill(temperature, windSpeed) {
+    if (temperature < 10 && windSpeed > 4.8) {
+        return 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
+    } else {
+        return null;
+    }
+}
+
+const temp = 20;
+const wind = 16;
+
+const windChill = calculateWindChill(temp, wind);
+
+if (windChill !== null) {
+    const windchillvalue = document.querySelector("#windchillvalue");
+    windchillvalue.innerHTML = `<span>${windChill}</span>`;
+}
+else {
+    const windchillvalue = document.querySelector("#windchillvalue");
+    windchillvalue.innerHTML = `<span>N/A<span>`;
+}
